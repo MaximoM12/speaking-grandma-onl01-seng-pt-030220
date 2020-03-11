b89a4e5aac1b12c615dba75fbfd151a5597d2064
Joseph Maximo Marguello <marguellomax@gmail.com>
2020-03-11 20:11:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 speak_to_grandma = 'HUH?! SPEAK UP, SONNY!'
 
-if speak_to_grandma(sayhi)
+if speak_to_grandma(say_hi)
   say_hi == "Hi Nana, how are you?"
   puts "HUH?! SPEAK UP, SONNY!"
 elsif == "I LOVE YOU GRANDMA!"
