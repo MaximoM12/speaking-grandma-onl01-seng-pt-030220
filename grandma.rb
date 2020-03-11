fb99658dd98aac1d297205d66beb01c62167f102
Joseph Maximo Marguello <marguellomax@gmail.com>
2020-03-11 22:20:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,7 +19,7 @@ if sayhi == "Hi Nana, how are you?"
    "HUH?! SPEAK UP, SONNY!"
 elsif sayhi == "I LOVE YOU GRANDMA!"
    "I LOVE YOU TOO PUMPKIN!"
-elsif sayhi.upcase == "Hi!"
+elsif sayhi.upcase == "HI!"
    "HUH?! SPEAK UP, SONNY!"
 else 
   "NO, NOT SINCE 1938!"
